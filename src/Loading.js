0412f90bdd1c4cbe7e8b477412b6acfcb34e2244
Vansh <vanshk605@gmail.com>
2023-04-29 18:16:19 +0530

    DDDDs

---
+++ src/Loading.js
@@ -0,0 +1,14 @@
+export default function Loading(){
+    return (
+        
+        <section class="dots-container">
+        <div class="dot"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+      </section>
+      
+            )
+    
+}
---
+++ src/components/home.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+// import { Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+export default function Home(props){
+    function displayError (){
+        setError(
+          
+              <div class="alert alert-danger alert-dismissible fade show" role="alert">
+              <strong>Input field cannot be empty</strong> 
+              <button type="button" onClick={()=>setError(["DDDD"])} class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+         
+           </div>
+    
+        )
+      }
+      const history = useNavigate();
+      const [error,setError] = useState([]);
+      const [QrImgLink] = useState([])
+    return(
+        <>
+           <div style={{position:"fixed" , top:"0"}} id="error">
+        {error}
+      </div>
+    <div className="Center">
+
+      <div style={{display:"flex", flexFlow:"column"}}>
+      <h1>Generate QR </h1>
+      <small style={{fontWeight: "650",color: "#67b587"}}>Generate a QR code in seconds for your business, event, or personal use.</small>  </div>
+      <input autoComplete="off" type="text" id="link" placeholder="Drop you link here." />
+      
+      
+      <button className="Generate" onClick={()=>{
+          let c = document.getElementById("link").value 
+          if(c === ""){
+              displayError()
+              return
+
+            }
+            
+            setTimeout(async() => {
+                
+                history("/GenerateQr/" + c);
+          
+        }, 500);
+      }}>
+        <span>
+          Generate
+        </span>
+      </button>
+
+    </div>
+    {QrImgLink  }
+    <code id="mr">
+		<small>MADE WITH 💖 BY Vansh</small> <br/>
+<small>All Rights Reserved © <a style={{color: "blue"}} href="https://mrnaveenwoodoworks.w3spaces.com/"> Vansh</a> </small>
+
+</code>
+
+</>
+    )
+}
